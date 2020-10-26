d6be2f686574cfa6bbd657bc193e9975f3028992
TaniaAAAAAAA <tsntania@gmail.com>
2020-10-26 17:43:58 +0200

    ADO DataSet DataTable

---
+++ DataSet_DataTable_ADO_New/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace DataSet_DataTable_ADO_New
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.btnBook = new System.Windows.Forms.Button();
+            this.btnSaveToXml = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(787, 376);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(787, 376);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(775, 400);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 45);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(795, 405);
+            this.tabControl1.TabIndex = 0;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl1_SelectedIndexChanged);
+            // 
+            // btnBook
+            // 
+            this.btnBook.Location = new System.Drawing.Point(638, 5);
+            this.btnBook.Name = "btnBook";
+            this.btnBook.Size = new System.Drawing.Size(150, 34);
+            this.btnBook.TabIndex = 1;
+            this.btnBook.Text = "Delete Book";
+            this.btnBook.UseVisualStyleBackColor = true;
+            this.btnBook.Click += new System.EventHandler(this.btnBook_Click);
+            // 
+            // btnSaveToXml
+            // 
+            this.btnSaveToXml.Location = new System.Drawing.Point(470, 5);
+            this.btnSaveToXml.Name = "btnSaveToXml";
+            this.btnSaveToXml.Size = new System.Drawing.Size(150, 34);
+            this.btnSaveToXml.TabIndex = 2;
+            this.btnSaveToXml.Text = "Save to xml";
+            this.btnSaveToXml.UseVisualStyleBackColor = true;
+            this.btnSaveToXml.Click += new System.EventHandler(this.btnSaveToXml_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSaveToXml);
+            this.Controls.Add(this.btnBook);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button btnBook;
+        private System.Windows.Forms.Button btnSaveToXml;
+    }
+}
+
---
+++ DataSet_DataTable_ADO_New/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataSet_DataTable_ADO_New
+{
+    public partial class Form1 : Form
+    {
+            string connectionString = ConfigurationManager.ConnectionStrings["defaultConnection"].ConnectionString;
+        public Form1()
+        {
+            InitializeComponent();
+           
+            SqlConnection conn = new SqlConnection(connectionString);
+            CreateTabPages(conn);
+          //  dataGridView1.DataSource = table;
+            if (conn != null)
+                conn.Close();
+        }
+         
+        private DataTable FillTable(DataTable table, SqlConnection conn)
+        {
+            SqlCommand command = new SqlCommand($"Select * from {tabControl1.SelectedTab.Text}", conn);
+
+            try
+            {
+                conn.Open();
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        do
+                        {
+                            int line = 0;
+                            while (reader.Read())
+                            {
+                                if (line == 0)
+                                {
+                                    for (int i = 0; i < reader.FieldCount; i++)
+                                    {
+
+                                        table.Columns.Add(reader.GetName(i));
+                                    }
+                                    line++;
+                                }
+                                DataRow r = table.NewRow();
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    r[i] = reader[i];
+                                }
+                                table.Rows.Add(r);
+                            }
+                        } while (reader.NextResult());
+                    }
+
+                }
+            }
+            catch (SqlException e)
+            { }
+                return table;
+        }
+
+        private void CreateTabPages(SqlConnection connection)
+        {
+            
+
+           tabControl1.TabPages.Clear();
+            tabControl1.TabPages.Add("Hello in our App");
+            Label l = new Label();
+            l.Text = "Hello in our App";
+            l.Size = new Size(300, 100);
+            l.TextAlign = ContentAlignment.MiddleCenter;
+            l.BackColor = System.Drawing.Color.Green;
+          
+            
+
+
+            tabControl1.TabPages[0].Controls.Add(l);
+            try
+            {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand();
+                    command.CommandText = "SELECT name FROM sys.tables";
+                    command.Connection = connection;
+
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+
+                            tabControl1.TabPages.Add(reader[0].ToString());
+                            
+                        }
+                    }
+                    reader.Close();
+                connection.Close();
+
+            }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+        DataTable tableX;
+            int flag = 0;
+        private void tabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                DataGridView dataGridView = new DataGridView();
+                Size s = new Size(775, 365);
+                dataGridView.Size = s;
+         tableX = new DataTable();
+
+            tableX.TableName = tabControl1.SelectedTab.Text;
+
+                SqlConnection conn = new SqlConnection(connectionString);
+            if (flag == 0)
+            {
+                // tableX = new DataTable();
+
+                tableX = FillTable(tableX, conn);
+                dataGridView.DataSource = tableX;
+                tabControl1.SelectedTab.Controls.Add(dataGridView);
+                if (conn != null)
+                    conn.Close();
+                flag++;
+            }
+            else
+            {
+                tableX = FillTable(tableX, conn);
+                dataGridView.DataSource = tableX;
+                tabControl1.SelectedTab.Controls.Add(dataGridView);
+                if (conn != null)
+                    conn.Close();
+            }
+
+        }
+
+        private void btnBook_Click(object sender, EventArgs e) //Delete
+        {
+           // if (dataGridView1.SelectedIndex == -1)
+             //   return;
+        }
+
+        private void btnSaveToXml_Click(object sender, EventArgs e)
+        {
+           tableX.WriteXml(tableX.TableName +".xml");
+
+        }
+    }
+}
